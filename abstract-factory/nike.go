eadac155ad507d8010db1c627dacecce9d73ecf6
ramamimu <ahhmaad.blogger@gmail.com>
2023-11-16 23:29:42 +0700

    feat: abstract factory

---
+++ abstract-factory/nike.go
@@ -0,0 +1,26 @@
+package main
+
+type NikeShoe struct {
+	Shoe
+}
+
+type Nike struct {
+}
+
+func (n *Nike) makeShoe() IShoe {
+	return &NikeShoe{
+		Shoe: Shoe{
+			logo: "nike",
+			size: 14,
+		},
+	}
+}
+
+func (n *Nike) makeShirt() IShirt {
+	return &NikeShirt{
+		Shirt: Shirt{
+			logo: "nike",
+			size: 14,
+		},
+	}
+}
